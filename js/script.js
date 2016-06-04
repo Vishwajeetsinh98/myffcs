855a5275d79c87fabfd741dbb84ba3b5132fedcd
TechDetective <vishwajeetsinhjadeja4@gmail.com>
2016-06-04 17:39:58 +0530

    Refilling Search Boxes Added

--- js/script.js
+++ js/script.js
@@ -8,19 +8,20 @@ $(document).ready(function(){
   });
   $(".close").click(function(){
     $(".timetable-menu-list").slideUp(400);
-
     $(".timetable-title-panel").show();
-
   });
   $('.timetable-results-trigger').click(function(){
     $('.timetable-menu-list').slideUp("slow",function(){
       $('.timetable-results').slideDown("slow");
     });
   });
+  if($(window).width()>=768 && $(window).width()<=1024){
+    $('.timetable-menu-list .push-l4').removeClass('push-l4');
+  }
 
   var windowWidth = $(window).width();
   var windowHeight = $(window).height();
-  $(".custom-card-action").on("click",function(e){
+  $(document).on("click",'.custom-card-action',function(e){
     if($(this).hasClass("front")){
       $(this).closest($('.custom-card')).find($(".custom-card-front")).hide();
       $(this).closest($('.custom-card')).find($('.custom-card-edit')).fadeIn("slow");
@@ -30,6 +31,8 @@ $(document).ready(function(){
       if(windowWidth<=600){
         //$(this).closest($('.custom-card')).width($(this).closest($('.custom-card')).find($('.custom-card-edit')).width());
         $(this).closest($('.custom-card')).css('width','270px');
+      } else if(windowWidth>600 && windowWidth<=1024){
+        $(this).closest($('.custom-card')).css('width','30%');
       }
     } else{
       $(this).closest($('.custom-card')).find($(".custom-card-edit")).hide();
@@ -39,14 +42,14 @@ $(document).ready(function(){
       $(this).removeClass("edit");
     }
   });
-  $(".custom-card-action-active").on("click",function(e){
+  /*$(".custom-card-action-active").on("click",function(e){
     $(".custom-card-edit").hide();
     $(".custom-card-front").fadeIn("slow");
     $(this).html('<i class="fa fa-ellipsis-v" aria-hidden="true"></i>');
     $(this).addClass("custom-card-action-inactive",function(){
       $(this).removeClass("custom-card-action-active");
     });
-  });
+  });*/
 
   if($(window).width()>1100)
     $('.timetable').css("width","96%");
